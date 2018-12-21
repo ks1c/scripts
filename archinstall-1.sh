17b9fea2ac6e3dad2106d93572dd67d120c1311d
ks1c <ksic3784@gmail.com>
2018-12-20 21:19:13 -0300

    arch install part 1

---
+++ archinstall-1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Verifica se o primeiro e segundo argumentos sao vazios. -e interpreta \n
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+echo "-linux_partition -efi_partition"
+exit 1
+fi
+
+#Atualiza relógio
+timedatectl set-ntp true
+
+echo -n "Atualizando mirrorlist..."
+reflector --country Brazil --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 50 --number 20 --protocol http --sort rate >> /etc/pacman.d/mirrorlist 
+echo "pronto."
+
+echo -n "Formatando particao linux..."
+mkfs.ext4 $1
+echo "pronto."
+
+echo -n "Montando particao linux..."
+mount $1 /mnt
+echo "pronto."
+
+echo -n "Montando particao UFI..."
+mkdir /mnt/boot
+mount $2 /mnt/boot
+echo "pronto."
+
+echo -n "Instalando arch-linux..."
+pacstrap /mnt base base-devel grub efibootmgr os-prober
+genfstab -U /mnt > /mnt/etc/fstab
+echo "pronto."
+
+
+
+
