3b89a0d8f1182ab5ef762f5a784a49987cb27f2a
ks1c <ksic3784@gmail.com>
2018-12-24 18:17:56 -0300

    .

--- wifi_connect.sh
+++ wifi_connect.sh
@@ -5,10 +5,10 @@ echo "interface ssid psk"
 exit 1
 fi
 
-echo network={ >> /etc/wpa_supplicant/wifi
+echo network={ > /etc/wpa_supplicant/wifi
 echo ssid="\""$2"\"" >> /etc/wpa_supplicant/wifi
 echo psk="\""$3"\"" >> /etc/wpa_supplicant/wifi
 echo } >> /etc/wpa_supplicant/wifi
 
-wpa_supplicant -i $1 -c /etc/wpa_supplicant/wifi
+wpa_supplicant -i $1 -c /etc/wpa_supplicant/wifi &
 dhcpcd
