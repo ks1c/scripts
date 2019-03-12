c6e0b0053f151f9c1790466b9af00cfd4efb3880
ks1c <ksic3784@gmail.com>
2019-03-12 15:40:45 -0400

    .

---
+++ update_mirrors.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CONTENT=$(cat /etc/pacman.d/mirrorlist)
+BRMIRRORS=$(awk '/## Brazil/{getline; print}' /etc/pacman.d/mirrorlist)
+echo "$BRMIRRORS" >/etc/pacman.d/mirrorlist
+echo "$CONTENT" >/etc/pacman.d/mirrorlist
