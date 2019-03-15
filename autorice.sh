5a60b165c5a7b0583ec1f0e95c5abde32783224f
ks1c <ksic3784@gmail.com>
2019-03-15 16:51:06 -0400

    .

--- autorice.sh
+++ autorice.sh
@@ -11,4 +11,14 @@ if [ ! -d /home/$USERNAME/.config ]; then
     mkdir /home/$USERNAME/.config
 fi
 
+rm -rf /home/$USERNAME/.config/fontconfig
 ln -s /home/$USERNAME/dotfiles/fontconfig /home/$USERNAME/.config/fontconfig
+
+rm -rf /home/$USERNAME/.config/fish
+ln -s /home/$USERNAME/dotfiles/fish /home/$USERNAME/.config/fish
+
+rm -rf /home/$USERNAME/.bashrc
+ln -s /home/$USERNAME/dotfiles/.bashrc /home/$USERNAME/.bashrc
+
+rm -rf /home/$USERNAME/.bash_profile
+ln -s /home/$USERNAME/dotfiles/.bash_profile /home/$USERNAME/.bash_profile
