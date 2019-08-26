1bff282c2f5f334c1880243156c2b210d56cac93
ks1c <ksic3784@gmail.com>
2019-08-26 18:08:53 -0400

    refactoring

--- dwmbar.sh
+++ dwmbar.sh
@@ -1,98 +1,30 @@
 #!/bin/sh
 
-STATUS=""
-
 _time() {
-
-	_time=" $(date '+%H:%M')"
-
-	if [ "$STATUS" = "" ]; then
-		STATUS="$_time"
-	else
-		STATUS="$STATUS  $_time"
-	fi
+	echo -n " $(date '+%H:%M')"
 }
 
 _date() {
-
-	_date=" $(date '+%Y-%m-%d')"
-
-	if [ "$STATUS" = "" ]; then
-		STATUS="$_date"
-	else
-		STATUS="$STATUS  $_date"
-	fi
+	echo -n " $(date '+%Y-%m-%d')"
 }
 
 _disk_usage_home() {
-
-	_disk_usage_home=" $(df -h $HOME | awk 'NR==2 {print $4}')"
-
-	if [ "$STATUS" = "" ]; then
-		STATUS="$_disk_usage_home"
-	else
-		STATUS="$STATUS  $_disk_usage_home"
-	fi
+	echo -n " $(df -h $HOME | awk 'NR==2 {print $4}')"
 }
 
 _disk_usage_storage() {
-
-	_disk_usage_storage=" $(df -h /media/files | awk 'NR==2 {print $4}')"
-
-	if [ "$STATUS" = "" ]; then
-		STATUS="$_disk_usage_storage"
-	else
-		STATUS="$STATUS  $_disk_usage_storage"
-	fi
-}
-
-_updates() {
-
-	_updates=" 5"
-
-	if [ "$STATUS" = "" ]; then
-		STATUS="$_updates"
-	else
-		STATUS="$STATUS  $_updates"
-	fi
+	echo -n " $(df -h /media/files | awk 'NR==2 {print $4}')"
 }
 
 _volume() {
-
-	_volume=" $(amixer sget Master | awk -F"[][]" '/dB/ { print $2 }')"
-
-	if [ "$STATUS" = "" ]; then
-		STATUS="$_volume"
-	else
-		STATUS="$STATUS  $_volume"
-	fi
+	echo -n " $(amixer sget Master | awk -F"[][]" '/dB/ { print $2 }')"
 }
 
-[ -f ~/.tmprc ] && source ~/.tmprc
-
 if [ $1 = "refresh" ]; then
-
-	STATUS=""
-	#_updates
-	_volume
-	_disk_usage_storage
-	_disk_usage_home
-	_date
-	_time
-	xsetroot -name "$STATUS"
-
+	xsetroot -name "$(_volume)  $(_disk_usage_storage)  $(_disk_usage_home)  $(_date)  $(_time)"
 else
-
 	while true; do
-
-		STATUS=""
-		#_updates
-		_volume
-		_disk_usage_storage
-		_disk_usage_home
-		_date
-		_time
-		xsetroot -name "$STATUS"
+		xsetroot -name "$(_volume)  $(_disk_usage_storage)  $(_disk_usage_home)  $(_date)  $(_time)"
 		sleep 1m
 	done
 fi
